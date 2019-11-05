b9f0bcdf7da6315057ea92e6dc8a282d14f80197
Louis Bergelson <louisb@broadinstitute.org>
2019-11-05 13:30:38 -0500

    push push push

--- Reporter.py
+++ Reporter.py
@@ -1,14 +1,12 @@
-from github import Github
+from github import Github, IssueComment, PullRequest
 import os
 import sys
 
 key = os.environ["GITHUB_TOKEN"]
 g = Github(key)
 
-ME=os.environ["GITHUB_USERNAME"]
-MAGIC="Travis Status:"
-
-
+ME = g.get_user().login
+MAGIC = "Travis Status:"
 
 pull_number = os.environ["TRAVIS_PULL_REQUEST"]
 repo_name = os.environ["TRAVIS_REPO_SLUG"]
@@ -20,51 +18,40 @@ repo = g.get_repo(repo_name)
 pull = repo.get_pull(int(pull_number))
 comments = pull.get_issue_comments()
 
-"""
-Status: 101
-Job: 3.1 url 
-Job: 3.2: url
-"""
 
-
-def matches_build_number(comment, build_number):
+def matches_build_number(comment: IssueComment, build_number: str) -> bool:
     body = comment.body
     lines = body.splitlines()
-    comment_build_number = lines[0].split(":")[1].strip()
-    return comment_build_number == build_number
+    return build_number in lines[0]
 
 
-def isMyComment(comment):
+def is_my_comment(comment: IssueComment) -> bool:
     return comment.user.login == ME
 
-travis_page_url = "https://travis-ci.com/"+ repo_name + "/builds/" + build_id
+
+travis_page_url = "https://travis-ci.com/" + repo_name + "/builds/" + build_id
 job_page_url = "https://travis-ci.com/" + repo_name + "/jobs/" + job_id
-report_path = os.environ["REPORT_PATH"]
-logs_storage = os.environ["HELLBENDER_TEST_LOGS"]
-log_url = "https://storage.googleapis.com" + logs_storage + report_path + "tests/test/index.html"
 
-message = "[%s](%s) - [Logs](%s)" % (job_number, job_page_url, sys.argv[1])
+log_url = sys.argv[1]
+message = "[%s](%s) - [Logs](%s)" % (job_number, job_page_url, log_url)
 
 
-def update(comment):
-    comment.edit(comment.body +"\n" + message)
-
-def newComment(pull):
-    pull.create_issue_comment(MAGIC + " " + build_number +
-                              "\nFailures in job [%s](%s)" % (build_number, travis_page_url)
-                              +"\n" + message)
+def update(comment: IssueComment) -> None:
+    comment.edit(comment.body + "\n" + message)
 
 
-#Update the comment or create a new one
+def new_comment(pull: PullRequest) -> None:
+    pull.create_issue_comment("Travis reported job failures from build [%s](%s)" % (build_number, travis_page_url)
+                              + "\nFailures in the following jobs:"
+                              + "\n" + message)
+
+
+# Update the comment or create a new one
 for comment in comments:
-    if isMyComment(comment) and matches_build_number(comment, build_number):
+    if is_my_comment(comment) and matches_build_number(comment, build_number):
         print("found matching comment")
         update(comment)
         break
 else:
-    newComment(pull)
-
-
-
-
-
+    print("creating new comment")
+    new_comment(pull)
